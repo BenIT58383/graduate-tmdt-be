cb102f239e1744f3fe770e203b77288171435bc8
benit58383 <benit58383@gmail.com>
2022-07-14 23:29:13 +0700

    fix

--- src/api/user/controller.js
+++ src/api/user/controller.js
@@ -32,7 +32,7 @@ const login = async (req, res, next) => {
     .login(phone, password)
     .then((data) => {
       return new APISuccess(res, {
-        data: data,
+        data: data.user.token,
       })
     })
     .catch((err) => {

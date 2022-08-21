1315b628007cf2a7b4e4d20aec57ec73b315eef4
benit58383 <benit58383@gmail.com>
2022-08-22 00:20:53 +0700

    ok

--- src/api/store/services.js
+++ src/api/store/services.js
@@ -23,6 +23,7 @@ import config from '../../common/config'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
 import jwtHelper from '../../common/helpers/jwt-helper'
+import dayjs from 'dayjs'
 
 const createStore = async (userId, name, image1, image2, image3, description, linkSupport, createdBy) => {
   const res = {}
@@ -106,6 +107,13 @@ const getListStore = async (page, size, name, userId, isActive) => {
   const data = await Sequelize.query(queryString, {
     type: Sequelize.QueryTypes.SELECT,
   })
+
+  if (data && data.length > 0) {
+    for (let item of data) {
+      item.createdAt = dayjs(item.createdAt).format('DD/MM/YYYY HH:mm:ss')
+      item.updatedAt = dayjs(item.updatedAt).format('DD/MM/YYYY HH:mm:ss')
+    }
+  }
 
   res.total = data.length
   res.stores = data.slice(offset, offset + size)
